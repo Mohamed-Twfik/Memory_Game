029407dd54a97a96dc133274a4ee1880840c5df5
mohamed_twfik <mohamedtwfik910@gmail.com>
2023-05-27 21:56:32 +0300

    implement start game , leave game, logout Events

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const cors = require('cors')
 const http = require('http').createServer(app)
 const io = new Server(http, {cors: {origin: 'http://localhost:3000'}})
 const port = process.env.PORT || 8000
+const bodyParser = require('body-parser')
 let rooms = []
+let runningRooms = []
 
 http.listen(port, () => {console.log(`Listening at URL http://localhost:${port}`)})
 
@@ -17,8 +19,9 @@ app.use(cors())
 app.post('/', (req, res)=>{
     let username = req.body.name
 
-    if(username) res.status(200).json({username})
-    else res.status(400).json({error: 'No username'})
+    if(!username) return res.status(400).json({error: 'No username'})
+    
+    res.status(200).json({username})
 
     io.on('connection', (socket)=>{
         socket.username = username
@@ -35,19 +38,21 @@ app.post('/', (req, res)=>{
         })
 
         socket.on('joinGame', async(roomId)=>{
-            if(rooms.includes(roomId)){
+            if(!rooms.includes(roomId)) socket.emit('idWrong', {roomId})
+
+            else if(runningRooms.includes(roomId)) socket.emit('roomStartRunning', roomId)
+
+            else{
                 let sockets = await io.in(roomId).fetchSockets()
                 roomFull = sockets.length >= 2
+
                 if(sockets.length < 6){
                     socket.roomId = roomId
                     socket.turns = 0
                     socket.join(roomId)
-                    socket.emit('joinWaitingRoom', {roomId, roomFull, username: socket.username, userId: socket.id, owner: false})
-                }else{
-                    socket.emit('roomFull', roomId)
+                    io.in(roomId).emit('joinWaitingRoom', {roomId, playersNumber: sockets.length, roomFull, username: socket.username, userId: socket.id, owner: false})
                 }
-            }else{
-                socket.emit('idWrong', {roomId})
+                else socket.emit('roomFull', roomId)
             }
         })
 
@@ -55,13 +60,14 @@ app.post('/', (req, res)=>{
             let roomId = socket.roomId
             let players = await io.in(roomId).fetchSockets()
             playersInfo = []
-            console.log(`${roomId} has ${players.length} players and started the game`)
+            console.log(`${roomId} has ${players.length} players and they started playing`)
             players.forEach((player)=>{
                 playersInfo.push({username: player.username, userId: player.id, turns: 0})
             })
             io.in(roomId).emit('startGame', {roomId, playersInfo})
+            runningRooms.push(roomId)
         })
-
+// -----------------------------------------------
         socket.on('turn', async(data)=>{
             let roomId = socket.roomId
             let players = await io.in(roomId).fetchSockets()
@@ -69,6 +75,29 @@ app.post('/', (req, res)=>{
             player.turns++
             socket.turns++
             io.in(roomId).emit('turn', {roomId, data})
+        })
+
+        socket.on('leaveGame', async()=>{
+            let roomId = socket.roomId
+            io.in(roomId).emit('playerLeft', {roomId, username: socket.username})
+            socket.leave(roomId)
+            let sockets = await io.in(roomId).fetchSockets()
+            if(sockets.length == 0){
+                rooms.splice(rooms.indexOf(roomId), 1)
+            }
+        })
+
+        // Disconnected
+        socket.on('logout', async() => {
+            console.log(socket.username + ' disconnected')
+            let roomId = socket.roomId
+            io.in(roomId).emit('playerLeft', {roomId: roomId, username: socket.username})
+            socket.leave(roomId)
+            let sockets = await io.in(roomId).fetchSockets()
+            if(sockets.length == 0){
+                rooms.splice(rooms.indexOf(roomId), 1)
+            }
+            socket.disconnect()
         })
 
     })
