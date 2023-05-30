2d45e0ba43f81a0e9bb354daafac7d432d7008cb
mohamed_twfik <mohamedtwfik910@gmail.com>
2023-05-31 00:14:36 +0300

    update socket url

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -9,7 +9,7 @@ export const SocketProvider = ({ children }) => {
 
 
   useEffect(() => {
-    const newSocket = io('http://localhost:8000');
+    const newSocket = io('https://cards-game-server.onrender.com');
     setSocket(newSocket);
 
     return () => newSocket.close();
